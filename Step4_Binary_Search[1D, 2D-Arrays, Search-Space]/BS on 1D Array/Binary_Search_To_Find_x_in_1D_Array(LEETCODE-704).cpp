1ebb058c067f919730045c0faf7aa1475a257a0e
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-15 13:02:14 +0530

    Update Binary_Search_To_Find_x_in_1D_Array(LEETCODE-704).cpp

--- Step4_Binary_Search[1D, 2D-Arrays, Search-Space]/BS on 1D Array/Binary_Search_To_Find_x_in_1D_Array(LEETCODE-704).cpp
+++ Step4_Binary_Search[1D, 2D-Arrays, Search-Space]/BS on 1D Array/Binary_Search_To_Find_x_in_1D_Array(LEETCODE-704).cpp
@@ -4,11 +4,18 @@ public:
         int n=nums.size();
         int low=0;
         int high=n-1;
-        int mid=(low+high)/2;
         while(low<=high){
+            int mid=(low+high)/2;
             if(nums[mid]==target){
                 return mid;            
             }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
+        return -1;
     }
 };
