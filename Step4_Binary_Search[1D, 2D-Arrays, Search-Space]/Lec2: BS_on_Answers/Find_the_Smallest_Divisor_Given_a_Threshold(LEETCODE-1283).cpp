481131430c61f79783fad5670bb3c2d62308f403
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-16 01:17:26 +0530

    Update Find_the_Smallest_Divisor_Given_a_Threshold(LEETCODE-1283).cpp

--- Step4_Binary_Search[1D, 2D-Arrays, Search-Space]/Lec2: BS_on_Answers/Find_the_Smallest_Divisor_Given_a_Threshold(LEETCODE-1283).cpp
+++ Step4_Binary_Search[1D, 2D-Arrays, Search-Space]/Lec2: BS_on_Answers/Find_the_Smallest_Divisor_Given_a_Threshold(LEETCODE-1283).cpp
@@ -1,1 +1,43 @@
-
+class Solution {
+public:
+    int maxn(vector<int>& arr){
+        int max=INT_MIN;
+        for(int val:arr){
+            if(val>max){
+                max=val;
+            }
+        }
+        return max;
+    }
+    bool possible(vector<int>& arr,int mid,int thres){
+        long long total=0;
+        for(int val:arr){
+            if(val==0){
+                continue;
+            }
+            total+=ceil((double)val/mid);
+        }
+        if(total<=thres){
+            return true;
+        }
+        return false;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int n=nums.size();
+        if(n>threshold) return -1;
+        int low=1;
+        int ans=INT_MAX;
+        int high=maxn(nums);
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(possible(nums,mid,threshold)==true){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
