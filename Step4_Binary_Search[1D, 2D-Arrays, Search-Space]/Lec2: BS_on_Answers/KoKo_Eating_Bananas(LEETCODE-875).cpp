4770394f998975ec34a6a10cd7c78a2a165528f1
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-15 23:21:11 +0530

    Update KoKo_Eating_Bananas(LEETCODE-875).cpp

--- Step4_Binary_Search[1D, 2D-Arrays, Search-Space]/Lec2: BS_on_Answers/KoKo_Eating_Bananas(LEETCODE-875).cpp
+++ Step4_Binary_Search[1D, 2D-Arrays, Search-Space]/Lec2: BS_on_Answers/KoKo_Eating_Bananas(LEETCODE-875).cpp
@@ -1,1 +1,39 @@
+class Solution {
+public:
+    
+    int maxelement(vector<int>& arr){
+        int maxv=INT_MIN;
+        for(int val:arr){
+            if(val>maxv){
+                maxv=val;
+            }
+        }
+        return maxv;
+    }
 
+    long long totaltime(vector<int>& arr,int mid){
+        long long totalhrs=0;
+        for(int val:arr){
+            totalhrs+=(long long)ceil((double)val/mid);
+        }
+        return totalhrs;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        int low=1;
+        int ans= INT_MAX;
+        int high=maxelement(piles);
+        while(low<=high){
+            int mid=(low+high)/2;
+            long long total=totaltime(piles,mid);
+            if(total<=h){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
