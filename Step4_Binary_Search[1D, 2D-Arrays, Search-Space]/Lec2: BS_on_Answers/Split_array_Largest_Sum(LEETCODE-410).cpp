e011eb20961158a44f99cb2bb0519da5e9c6e581
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-16 16:09:29 +0530

    Update Split_array_Largest_Sum(LEETCODE-410).cpp

--- Step4_Binary_Search[1D, 2D-Arrays, Search-Space]/Lec2: BS_on_Answers/Split_array_Largest_Sum(LEETCODE-410).cpp
+++ Step4_Binary_Search[1D, 2D-Arrays, Search-Space]/Lec2: BS_on_Answers/Split_array_Largest_Sum(LEETCODE-410).cpp
@@ -1,1 +1,37 @@
-
+class Solution {
+public:
+    int sumn(vector<int>& arr){
+        int sum=0;
+        for(int val:arr){
+            sum+=val;
+        }
+        return sum;
+    }
+    int subarraysum(vector<int>&arr,int mid){
+        int count=1;int last=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]+last>mid){
+                count++;
+                last=arr[i];
+            }
+            else{
+                last+=arr[i];
+            }
+        }
+        return count;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int low=*max_element(nums.begin(),nums.end());
+        int high =sumn(nums);
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(subarraysum(nums,mid)>k){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return low;
+    }
+};
