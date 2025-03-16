db314671d6be1ef2b0c6e1096b3a23b780345cf0
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-16 15:53:54 +0530

    Update Book_Allocation_Problem.cpp

--- Step4_Binary_Search[1D, 2D-Arrays, Search-Space]/Lec2: BS_on_Answers/Book_Allocation_Problem.cpp
+++ Step4_Binary_Search[1D, 2D-Arrays, Search-Space]/Lec2: BS_on_Answers/Book_Allocation_Problem.cpp
@@ -1,1 +1,36 @@
+    int sumn(vector<int>& arr){
+        int sum=0;
+        for(int val:arr){
+            sum+=val;
+        }
+        return sum;
+    }
+int countstudent(vector<int>&arr,int mid,int n){
+    int pages=0;int stu=1;
+    for(int i=0;i<n;i++){
+        if(arr[i]+pages>mid){
+            stu++;
+            pages=arr[i];
 
+        }
+        else{
+            pages+=arr[i];
+        }
+    }
+    return stu;
+}
+int findPages(vector<int>& arr, int n, int m) {
+    if(m>n) return -1;
+    int low=*max_element(arr.begin(),arr.end());
+    int high=sumn(arr);
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(countstudent(arr,mid,n)>m){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return low;
+}
