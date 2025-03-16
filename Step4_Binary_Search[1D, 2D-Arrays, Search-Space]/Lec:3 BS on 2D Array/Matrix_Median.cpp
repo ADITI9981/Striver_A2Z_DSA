0d2df9e11684a69c1c810cec1facd63bcd9de00d
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-17 01:21:05 +0530

    Update Matrix_Median.cpp

--- Step4_Binary_Search[1D, 2D-Arrays, Search-Space]/Lec:3 BS on 2D Array/Matrix_Median.cpp
+++ Step4_Binary_Search[1D, 2D-Arrays, Search-Space]/Lec:3 BS on 2D Array/Matrix_Median.cpp
@@ -1,1 +1,84 @@
+//{ Driver Code Starts
+// Initial template for C++
 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    
+    int upperbound(vector<int>& arr,int x,int m){
+        int low=0,high=m-1;
+        int ans=m;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    int smallnocount(vector<vector<int>> &mat,int n,int m,int mid){
+        int count=0;
+        for(int i=0;i<n;i++){
+            count+=upperbound(mat[i],mid,m);
+        }
+        return count;
+    }
+    int median(vector<vector<int>> &mat) {
+        int low=INT_MAX, high=INT_MIN;
+        int n=mat.size();
+        int m=mat[0].size();
+        for(int i=0;i<n;i++){
+            low=min(low,mat[i][0]);
+            high=max(high,mat[i][m-1]);
+        }
+        int req=(n*m)/2;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int count=smallnocount(mat,n,m,mid);
+            if(count<=req){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return low;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r, vector<int>(c));
+        for (int i = 0; i < r; ++i)
+            for (int j = 0; j < c; ++j)
+                cin >> matrix[i][j];
+        Solution obj;
+        int ans = -1;
+        ans = obj.median(matrix);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
