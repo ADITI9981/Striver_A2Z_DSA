b0d477be99e11c33f01273a7bb914dc4446f27a5
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-17 17:08:24 +0530

    Update check_whether_one_string_is_a_rotation_of_another(LEETCODE-796).cpp

--- Step5_Strings[Basics And Medium]/Lec1_Basic And Easy String Problem/check_whether_one_string_is_a_rotation_of_another(LEETCODE-796).cpp
+++ Step5_Strings[Basics And Medium]/Lec1_Basic And Easy String Problem/check_whether_one_string_is_a_rotation_of_another(LEETCODE-796).cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size()){
+            return false;
+        }
+        string combined=s+s;
+        if(combined.find(goal)==string::npos){
+            return false;
+        }
+        return true;
+    }
+};
