b73f878777991d7b367482a95610635a30537d77
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-17 14:48:00 +0530

    Update Remove_outermost_Paranthesis(LEETCODE-1021).cpp

--- Step5_Strings[Basics And Medium]/Lec1_Basic And Easy String Problem/Remove_outermost_Paranthesis(LEETCODE-1021).cpp
+++ Step5_Strings[Basics And Medium]/Lec1_Basic And Easy String Problem/Remove_outermost_Paranthesis(LEETCODE-1021).cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int present=0;
+        string result;
+        for(char str:s){
+            if(str=='('){
+                if(present>0){
+                result.push_back(str);
+               }
+                present++;
+            }
+            else if(str==')'){
+                present--;
+                if(present>0){
+                    result.push_back(str);
+                }
+            }
+        }
+        return result;
+    }
+};
