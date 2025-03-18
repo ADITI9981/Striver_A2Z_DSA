61e0aee8d10fe7d6358f17405b2b684ae548dd94
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-19 01:18:22 +0530

    Update Sort_Characters_by_frequency(LEETCODE-451).cpp

--- Step5_Strings[Basics And Medium]/Lec2_Medium String Problem/Sort_Characters_by_frequency(LEETCODE-451).cpp
+++ Step5_Strings[Basics And Medium]/Lec2_Medium String Problem/Sort_Characters_by_frequency(LEETCODE-451).cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> freq;
+        for (auto c : s) {
+            freq[c]++;
+        }
+        vector<vector<char>> bucketdata(s.length()+1);
+        for(auto [c,f]:freq) bucketdata[f].push_back(c);
+        string  result="";
+        for(int i=s.length();i>=1;i--){
+            for(auto ch:bucketdata[i]){
+                result.append(i,ch);
+            }
+        }
+        return result;
+    }
+};
