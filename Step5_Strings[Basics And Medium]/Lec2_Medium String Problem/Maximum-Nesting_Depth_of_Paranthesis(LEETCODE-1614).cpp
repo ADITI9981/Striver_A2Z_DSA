5e638c46d34703bad65334eb72f7ea72c48b9c9d
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-19 23:51:28 +0530

    Update Maximum-Nesting_Depth_of_Paranthesis(LEETCODE-1614).cpp

--- Step5_Strings[Basics And Medium]/Lec2_Medium String Problem/Maximum-Nesting_Depth_of_Paranthesis(LEETCODE-1614).cpp
+++ Step5_Strings[Basics And Medium]/Lec2_Medium String Problem/Maximum-Nesting_Depth_of_Paranthesis(LEETCODE-1614).cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int maxDepth(string s) {
+        int open=0;
+        int result=0;
+        for(int ch:s){
+            if(ch=='('){
+                open++;
+            }
+            else if(ch==')'){
+                open--;         
+            }
+            result=max(result,open);
+        }
+        return result;
+    }
+};
