0ab534615c2ecfa7f29335af26a3377dc3db9047
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-20 00:15:26 +0530

    Update Roman_Number_to_Integer_and_vice_versa(LEETCODE-13).cpp

--- Step5_Strings[Basics And Medium]/Lec2_Medium String Problem/Roman_Number_to_Integer_and_vice_versa(LEETCODE-13).cpp
+++ Step5_Strings[Basics And Medium]/Lec2_Medium String Problem/Roman_Number_to_Integer_and_vice_versa(LEETCODE-13).cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> romanMap = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, 
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        int currentval=0;
+        int prevval=0;
+        int total=0;
+        for(int i=s.size()-1;i>=0;i--){
+            currentval=romanMap[s[i]];
+            if(currentval<prevval){
+                total-=currentval;
+            }
+            else{
+                total+=currentval;
+            }
+            prevval=currentval;
+        }
+        return total;
+    }
+};
