26f82f8f97276470b221f064a7b72dc804d57f9f
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-24 21:49:45 +0530

    Update Implement_Min_Stack(LEETCODE-155).cpp

--- Step9: Stack And Queues[Learning,Pre-in-Post-fix,Monotonic Stack,implimentation]/Lec1: Learning/Implement_Min_Stack(LEETCODE-155).cpp
+++ Step9: Stack And Queues[Learning,Pre-in-Post-fix,Monotonic Stack,implimentation]/Lec1: Learning/Implement_Min_Stack(LEETCODE-155).cpp
@@ -1,1 +1,41 @@
+class MinStack {
+private:
+    stack<int> s1;
+    stack<int> minstack;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s1.push(val);
+        if(minstack.empty()||val<=minstack.top()){
+            minstack.push(val);
+        }
+    }
+    
+    void pop() {
+        if(s1.top()==minstack.top()){
+            minstack.pop();
+        }
+        s1.pop();
+    }
+    
+    int top() {
+        int val=s1.top();
+        return val;
+    }
+    
+    int getMin() {
+      return minstack.top();
+    }
+};
 
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
