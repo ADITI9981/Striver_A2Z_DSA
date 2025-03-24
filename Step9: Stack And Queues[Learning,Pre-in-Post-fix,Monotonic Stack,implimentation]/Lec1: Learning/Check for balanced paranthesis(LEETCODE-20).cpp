f033cbf9fcd92e5763112b54244cc00c06dc4b1f
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-24 21:36:19 +0530

    Update Check for balanced paranthesis(LEETCODE-20).cpp

--- Step9: Stack And Queues[Learning,Pre-in-Post-fix,Monotonic Stack,implimentation]/Lec1: Learning/Check for balanced paranthesis(LEETCODE-20).cpp
+++ Step9: Stack And Queues[Learning,Pre-in-Post-fix,Monotonic Stack,implimentation]/Lec1: Learning/Check for balanced paranthesis(LEETCODE-20).cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> s1;
+        for(char i: s){
+            if(i=='('||i=='{'||i=='['){
+                s1.push(i);
+            }
+            else{
+                if(s1.empty()){
+                    return false;
+                }
+                char top = s1.top();
+                if((i == ')' && top == '(') || 
+                   (i == '}' && top == '{') || 
+                   (i == ']' && top == '[')) {
+                    s1.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return s1.empty();
 
+    }
+};
