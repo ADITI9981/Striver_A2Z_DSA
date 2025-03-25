7c7125f273dcc518dcdd0d667147b46b9832de39
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-25 21:18:25 +0530

    Update Next_Smaller_Element.cpp

--- Step9: Stack And Queues[Learning,Pre-in-Post-fix,Monotonic Stack,implimentation]/Lec3: Monotonic Stack&Queue Problems/Next_Smaller_Element.cpp
+++ Step9: Stack And Queues[Learning,Pre-in-Post-fix,Monotonic Stack,implimentation]/Lec3: Monotonic Stack&Queue Problems/Next_Smaller_Element.cpp
@@ -1,1 +1,20 @@
+vector<int> Solution::prevSmaller(vector<int> &A) {
+        stack<int> st;
+        int n=A.size();
+        vector<int> result(n);
+        for (int i =0 ; i < n; i++) {
+            int num = A[i];
+            while (!st.empty() && st.top() >= num) {
+                st.pop();
+            }
+            if (st.empty()) {
+                result[i] = -1;
+            } else {
+                result[i] = st.top();
+            }
+            st.push(num);
+        }
+        return result;
+        
+}
 
