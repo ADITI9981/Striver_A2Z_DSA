b2a93bb268fbe0f7aeb4b2475887bbf6d31ef21d
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-25 16:35:25 +0530

    Update Infix_to_Postfix_Conversion_using_Stack.cpp

--- Step9: Stack And Queues[Learning,Pre-in-Post-fix,Monotonic Stack,implimentation]/Lec2: Prefix, Infix & Postfix Conversion Problem/Infix_to_Postfix_Conversion_using_Stack.cpp
+++ Step9: Stack And Queues[Learning,Pre-in-Post-fix,Monotonic Stack,implimentation]/Lec2: Prefix, Infix & Postfix Conversion Problem/Infix_to_Postfix_Conversion_using_Stack.cpp
@@ -1,1 +1,77 @@
+//{ Driver Code Starts
+// C++ implementation to convert infix expression to postfix
+#include <bits/stdc++.h>
+using namespace std;
 
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+  
+    int priority(char c) {
+        if (c == '^') return 3;
+        else if (c == '*' || c == '/') return 2;
+        else if (c == '+' || c == '-') return 1;
+        else return -1;
+    }
+    // Function to convert an infix expression to a postfix expression.
+    string infixToPostfix(string& s) {
+        int n=s.size();
+        int i=0;
+        stack<char> st;
+        string ans="";
+        
+        
+        while(i<n){
+            if(isalnum(s[i])){
+                ans+=s[i];
+            }
+            else if(s[i]=='('){
+                st.push(s[i]);
+            }
+            else if(s[i]==')'){
+                while(!st.empty()&&st.top()!='('){
+                    ans+=st.top();
+                    st.pop();
+                }
+                st.pop();
+            }
+            else{
+                while(!st.empty() && priority(s[i])<=priority(st.top())){
+                    ans+=st.top();
+                    st.pop();
+                }
+                st.push(s[i]);
+            }
+            i++;
+        }
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+//  Driver program to test above functions
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(INT_MAX, '\n');
+    while (t--) {
+        string exp;
+        cin >> exp;
+        Solution ob;
+        cout << ob.infixToPostfix(exp) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
