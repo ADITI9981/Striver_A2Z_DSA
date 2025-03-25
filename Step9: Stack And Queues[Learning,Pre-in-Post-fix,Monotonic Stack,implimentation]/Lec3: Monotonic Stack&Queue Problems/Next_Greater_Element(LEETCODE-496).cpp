e2908be3e1145a0fc40e8aa6d4e5f02617fbe85e
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-25 19:23:43 +0530

    Update Next_Greater_Element(LEETCODE-496).cpp

--- Step9: Stack And Queues[Learning,Pre-in-Post-fix,Monotonic Stack,implimentation]/Lec3: Monotonic Stack&Queue Problems/Next_Greater_Element(LEETCODE-496).cpp
+++ Step9: Stack And Queues[Learning,Pre-in-Post-fix,Monotonic Stack,implimentation]/Lec3: Monotonic Stack&Queue Problems/Next_Greater_Element(LEETCODE-496).cpp
@@ -1,1 +1,28 @@
-
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> nextGreater;
+        stack<int> st;
+        
+        for (int i = nums2.size() - 1; i >= 0; --i) {
+            int num = nums2[i];
+            while (!st.empty() && st.top() <= num) {
+                st.pop();
+            }
+            if (st.empty()) {
+                nextGreater[num] = -1;
+            } else {
+                nextGreater[num] = st.top();
+            }
+            st.push(num);
+        }
+        
+        // Prepare the result for nums1
+        vector<int> result;
+        for (int num : nums1) {
+            result.push_back(nextGreater[num]);
+        }
+        
+        return result;
+    }
+};
