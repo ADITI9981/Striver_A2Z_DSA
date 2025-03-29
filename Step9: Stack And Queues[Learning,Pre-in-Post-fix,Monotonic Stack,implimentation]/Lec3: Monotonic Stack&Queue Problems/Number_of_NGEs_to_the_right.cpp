f8a9a16efde12f373e860eafae659aa5bf12aa80
Aditi Yadav <munmunyadav9981@gmail.com>
2025-03-29 12:45:56 +0530

    Update Number_of_NGEs_to_the_right.cpp

--- Step9: Stack And Queues[Learning,Pre-in-Post-fix,Monotonic Stack,implimentation]/Lec3: Monotonic Stack&Queue Problems/Number_of_NGEs_to_the_right.cpp
+++ Step9: Stack And Queues[Learning,Pre-in-Post-fix,Monotonic Stack,implimentation]/Lec3: Monotonic Stack&Queue Problems/Number_of_NGEs_to_the_right.cpp
@@ -1,1 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
 
+
+// } Driver Code Ends
+
+//User function Template for C++
+
+class Solution{
+public:
+
+    vector<int> count_NGE(int n, vector<int> &arr, int queries, vector<int> &indices){
+        vector<int> result;
+        stack<int> st;
+        vector<int> greatercount(n,0);
+        multiset<int> sortedlist;
+        for(int i=n-1;i>=0;i--){
+            greatercount[i]=distance(sortedlist.upper_bound(arr[i]),sortedlist.end());
+            sortedlist.insert(arr[i]);
+        }
+        for(int i:indices){
+            result.push_back(greatercount[i]);
+        }
+        return result;
+        
+    }
+
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while(t--){
+        //Input
+
+       int n; cin >> n;
+       vector<int> v1(n);
+       for(int i = 0;i<n;i++) cin >> v1[i];
+        int q; cin >> q;
+        vector<int> v2(q);
+        for(int i = 0;i<q;i++) cin >> v2[i];
+
+        Solution obj;
+        vector<int> ans = obj.count_NGE(n,v1,q,v2);
+
+        for(int i = 0;i<ans.size();i++)
+            cout << ans[i] << " ";
+        cout << endl;
+
+        
+        // cout << "~\n";
+    
+cout << "~" << "\n";
+}
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
