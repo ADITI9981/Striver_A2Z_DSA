93a5f909e1f09fb5f5e80aa0f394e03f6bb8f5d7
Aditi Yadav <munmunyadav9981@gmail.com>
2025-04-05 20:18:30 +0530

    Update Find_the_two_numbers_appearing_odd_number_of_times.cpp

--- Step8_Bit_Manipulation[Concepts & Problems]/Lec2: Interview Problems/Find_the_two_numbers_appearing_odd_number_of_times.cpp
+++ Step8_Bit_Manipulation[Concepts & Problems]/Lec2: Interview Problems/Find_the_two_numbers_appearing_odd_number_of_times.cpp
@@ -1,1 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
 
+// } Driver Code Ends
+
+//User function Template for C++
+class Solution{
+    public:
+    vector<long long int> twoOddNum(long long int Arr[], long long int N)  
+    {
+        long long int xorall=0;
+        for(int i=0;i<N;i++){
+            xorall^=Arr[i];
+        }
+        long long int rightmost=xorall&-xorall;
+        
+        long long int b1=0;
+        long long int b2=0;
+        
+        for(int i=0;i<N;i++){
+            if(Arr[i]&rightmost){
+                b1^=Arr[i];
+            }
+            else{
+                b2^=Arr[i];
+            }
+        }
+        if (b1 > b2)
+            return {b1, b2};
+        else
+            return {b2, b1};
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int N;
+        cin>>N;
+        long long int Arr[N];
+        for(long int i = 0;i < N;i++)
+        cin>>Arr[i];
+        Solution ob;
+        vector<long long int>ans=ob.twoOddNum(Arr,N);
+        for(long long int i=0;i<ans.size();i++)cout<<ans[i]<<" ";
+        cout<<endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
