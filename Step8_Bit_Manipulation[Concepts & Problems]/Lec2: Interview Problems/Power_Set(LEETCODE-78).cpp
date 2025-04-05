bff1520d0b1c573d6410d2e61808b1e3a2178d04
Aditi Yadav <munmunyadav9981@gmail.com>
2025-04-05 19:02:19 +0530

    Update Power_Set(LEETCODE-78).cpp

--- Step8_Bit_Manipulation[Concepts & Problems]/Lec2: Interview Problems/Power_Set(LEETCODE-78).cpp
+++ Step8_Bit_Manipulation[Concepts & Problems]/Lec2: Interview Problems/Power_Set(LEETCODE-78).cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int subset=1<<nums.size();
+        for(int i=0;i<subset;i++){
+            vector<int> s;
+            for(int j=0;j<31;j++){
+                if(i&(1<<j)){
+                    s.push_back(nums[j]);
+                }
+            }
+            ans.push_back(s);
+        }
+        return ans;
+    }
+};
