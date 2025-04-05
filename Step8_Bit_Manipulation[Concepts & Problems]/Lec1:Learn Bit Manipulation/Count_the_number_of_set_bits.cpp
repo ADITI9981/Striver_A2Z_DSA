c9ca91a1abf89d181fe352f9fa5e8c3f0c3d2e3c
Aditi Yadav <munmunyadav9981@gmail.com>
2025-04-05 18:18:19 +0530

    Update Count_the_number_of_set_bits.cpp

--- Step8_Bit_Manipulation[Concepts & Problems]/Lec1:Learn Bit Manipulation/Count_the_number_of_set_bits.cpp
+++ Step8_Bit_Manipulation[Concepts & Problems]/Lec1:Learn Bit Manipulation/Count_the_number_of_set_bits.cpp
@@ -1,1 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for C++
 
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+//User function Template for C++
+
+class Solution{
+    public:
+    // n: input to count the number of set bits
+    //Function to return sum of count of set bits in the integers from 1 to n.
+    int countSetBits(int n)
+    {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<31;j++){
+                if(i&(1<<j)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+	 int t;
+	 cin>>t;// input testcases
+	 while(t--) //while testcases exist
+	 {
+	       int n;
+	       cin>>n; //input n
+	       Solution ob;
+	       cout << ob.countSetBits(n) << endl;// print the answer
+	  
+cout << "~" << "\n";
+}
+	  return 0;
+}
+
+// } Driver Code Ends
