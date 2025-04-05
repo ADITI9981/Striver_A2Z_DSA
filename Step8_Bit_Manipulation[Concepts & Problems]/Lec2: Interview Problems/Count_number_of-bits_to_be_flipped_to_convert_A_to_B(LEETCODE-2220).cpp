90a5e68b4464eea2c7d36b623abfb5577c5f4c5e
Aditi Yadav <munmunyadav9981@gmail.com>
2025-04-05 18:05:42 +0530

    Update Count_number_of-bits_to_be_flipped_to_convert_A_to_B(LEETCODE-2220).cpp

--- Step8_Bit_Manipulation[Concepts & Problems]/Lec2: Interview Problems/Count_number_of-bits_to_be_flipped_to_convert_A_to_B(LEETCODE-2220).cpp
+++ Step8_Bit_Manipulation[Concepts & Problems]/Lec2: Interview Problems/Count_number_of-bits_to_be_flipped_to_convert_A_to_B(LEETCODE-2220).cpp
@@ -1,1 +1,13 @@
-Count number of bits to be flipped to convert A to B
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int x=start^goal;
+        int count=0;
+        for(int i=0;i<31;i++){
+            if(x&1<<i){
+                count++;
+            }
+        }
+        return count;
+    }
+};
