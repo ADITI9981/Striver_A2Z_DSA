aac182d90686fad88f4cdff32745d1939770f8ad
Aditi Yadav <munmunyadav9981@gmail.com>
2025-05-01 17:56:31 +0530

    Update Generate_Paranthesis(LEETCODE-22).cpp

--- Step7_Recursion_Patternwise/Lec2: Subsiquences_Pattern/Generate_Paranthesis(LEETCODE-22).cpp
+++ Step7_Recursion_Patternwise/Lec2: Subsiquences_Pattern/Generate_Paranthesis(LEETCODE-22).cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+  void print(int open, int close, string str, vector<string>& result, int n) {
+        if (str.length() == 2 * n) {
+            result.push_back(str);
+            return;
+        }
 
+        if (open < n) {
+            print(open + 1, close, str + '(', result, n);
+        }
+
+        if (close < open) {
+            print(open, close + 1, str + ')', result, n);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        print(0, 0, "", result, n);
+        return result;
+    }
+};
