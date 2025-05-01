71cec3ba8f5e0de6516f1dff42f837d4b256e2d8
Aditi Yadav <munmunyadav9981@gmail.com>
2025-05-01 14:12:24 +0530

    Update Count_Good_numbers(LEETCODE-1922).cpp

--- Step7_Recursion_Patternwise/Lec1: Get_A_Strong_Hold/Count_Good_numbers(LEETCODE-1922).cpp
+++ Step7_Recursion_Patternwise/Lec1: Get_A_Strong_Hold/Count_Good_numbers(LEETCODE-1922).cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
 
+    int power(long long x,long long N){
+        long long ans=1;
+        x%=MOD;
+        while (N > 0) {
+            if (N % 2 == 1) {
+                ans = (ans * x)%MOD;
+                N = N - 1;
+            } else {
+                x =(x * x)%MOD;
+                N = N / 2;
+            }
+        }
+        return ans;
+    }
+    int countGoodNumbers(long long n) {
+        long long odd_count = n/2;
+        long long even_count = n-(n/2);
+        long long even_part = power(5, even_count);
+        long long odd_part = power(4, odd_count);
+
+        return (even_part * odd_part) % MOD;
+    }
+    
+};
