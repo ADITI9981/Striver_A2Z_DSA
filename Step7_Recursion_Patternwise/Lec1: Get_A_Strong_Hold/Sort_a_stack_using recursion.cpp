9ae84116fe9ff9c5f6fd7b5936a53649a236ebf9
Aditi Yadav <munmunyadav9981@gmail.com>
2025-05-01 14:54:37 +0530

    Update Sort_a_stack_using recursion.cpp

--- Step7_Recursion_Patternwise/Lec1: Get_A_Strong_Hold/Sort_a_stack_using recursion.cpp
+++ Step7_Recursion_Patternwise/Lec1: Get_A_Strong_Hold/Sort_a_stack_using recursion.cpp
@@ -1,1 +1,63 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
 
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+
+cout << "~" << "\n";
+}
+}
+// } Driver Code Ends
+
+
+void SortedStack ::sort() {
+    int x;
+    stack<int> st;
+    while(!s.empty()){
+        x=s.top();
+        s.pop();
+        while(!st.empty() && st.top()>x){
+            int y=st.top();
+            st.pop();
+            s.push(y);
+        }
+        st.push(x);
+    }
+    
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+}
