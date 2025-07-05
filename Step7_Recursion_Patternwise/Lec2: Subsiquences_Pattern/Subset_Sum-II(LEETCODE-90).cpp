c68526f5c90630e73523e537e99a9cab9664b846
Aditi Yadav <munmunyadav9981@gmail.com>
2025-07-05 14:39:07 +0530

    Update Subset_Sum-II(LEETCODE-90).cpp

--- Step7_Recursion_Patternwise/Lec2: Subsiquences_Pattern/Subset_Sum-II(LEETCODE-90).cpp
+++ Step7_Recursion_Patternwise/Lec2: Subsiquences_Pattern/Subset_Sum-II(LEETCODE-90).cpp
@@ -1,1 +1,21 @@
+class Solution {
 
+public:
+    void func(int ind, vector<int> &arr, vector<int> &ds, vector<vector<int>> &ans){
+        ans.push_back(ds);
+        for(int i=ind;i<arr.size();i++){
+            if(i!=ind && arr[i]==arr[i-1]) continue;
+            ds.push_back(arr[i]);
+            func(i+1, arr, ds, ans);
+            ds.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(nums.begin(),nums.end());
+        func(0,nums,ds,ans);
+        return ans;
+    }
+};
