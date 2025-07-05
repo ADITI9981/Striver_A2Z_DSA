93b59f9f018de881901c646c5c44c46957e2b8a4
Aditi Yadav <munmunyadav9981@gmail.com>
2025-07-05 13:43:57 +0530

    Update Subset_Sum-I.cpp

--- Step7_Recursion_Patternwise/Lec2: Subsiquences_Pattern/Subset_Sum-I.cpp
+++ Step7_Recursion_Patternwise/Lec2: Subsiquences_Pattern/Subset_Sum-I.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+    
+  public:
+    void func(int ind,int sum,vector<int> &arr,int N,vector<int> &subsetsum){
+        if(ind==N){
+            subsetsum.push_back(sum);
+            return;
+        }
+        func(ind+1,sum+arr[ind],arr,N,subsetsum);
+        func(ind+1,sum,arr,N,subsetsum);
+    }
+  public:
+    vector<int> subsetSums(vector<int>& arr) {
+        // code here
+        int N = arr.size();
+        vector<int> subsetsum;
+        func(0,0,arr,N,subsetsum);
+        sort(subsetsum.begin(),subsetsum.end());
+        return subsetsum;
+    }
+};
