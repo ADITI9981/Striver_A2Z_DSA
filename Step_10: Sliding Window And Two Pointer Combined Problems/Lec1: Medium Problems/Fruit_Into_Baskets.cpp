344c4a8894a5fe6a0457332d9fd29babdbe25a0f
Aditi Yadav <munmunyadav9981@gmail.com>
2025-07-06 18:28:48 +0530

    Update Fruit_Into_Baskets.cpp

--- Step_10: Sliding Window And Two Pointer Combined Problems/Lec1: Medium Problems/Fruit_Into_Baskets.cpp
+++ Step_10: Sliding Window And Two Pointer Combined Problems/Lec1: Medium Problems/Fruit_Into_Baskets.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n=fruits.size();
+        int maxlen=0, l=0,r=0,len=0;
+        map<int,int> m;
+        while(r<n){
+            m[fruits[r]]++;
+            if(m.size() > 2){
+                m[fruits[l]]--;
+                if(m[fruits[l]] == 0) m.erase(fruits[l]);
+                l++;
+            }
+            if(m.size()<=2){
+                maxlen=max(maxlen,r-l+1);
 
+            }
+            r++;
+        }
+        return maxlen;
+    }
+};
